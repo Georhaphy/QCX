effa1bdb4f6ee57657e638ed023738192f6fe1a8
Georhaphy <137684498+Georhaphy@users.noreply.github.com>
2025-02-23 22:44:00 +0700

    Update QCX.py

--- QCX.py
+++ QCX.py
@@ -44,7 +44,7 @@ def path_to_eagertensor(image_path):
 
 st.markdown("<h1 style='text-align: center; color: black ; font-size: 25px ;'>Sakhon QCX</h1>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; color: black ; font-size: 19px ;'><em>Good quality  Good using</em></h1>", unsafe_allow_html=True)
-img_file = st.file_uploader("เปิดไฟล์ภาพ")
+img_file = st.file_uploader("เปิดไฟล์ภาพ", type='jpg')
 
 
 
